8f51dadebc8306cf24e95b353afe42f1ef8da9ab
Couzy, Guillaume <guillaumecouzy@eaton.com>
2018-12-07 13:35:27 +0100

    Integegrated Formation model into Formation component

--- src/app/formation/create-formation/create-formation.component.ts
+++ src/app/formation/create-formation/create-formation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Formation } from '../models/Formation.model';
 
 @Component({
   selector: 'app-create-formation',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFormationComponent implements OnInit {
 
+  @Input() // Ce qui rentre
+  formation: Formation;
+  // Si autres parametres in, placer le décorateur
+  // @Input()
+  // var2: Type;
+
+  @Output() // Ce qui sort
+  formationChange: EventEmitter<Formation> = new EventEmitter<Formation>();
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.formation = {
+        title: "Angulaaar"
+      }
+    }, 3000);
   }
-
 }
