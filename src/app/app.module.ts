8813ad257a393b21d70a80620230f35925cc61d3
Couzy, Guillaume <guillaumecouzy@eaton.com>
2018-12-07 12:15:52 +0100

    My first Angular component !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormationComponent } from './formation/formation.component';
+import { UtilisateurComponent } from './utilisateur/utilisateur.component';
+import { CreateFormationComponent } from './formation/create-formation/create-formation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormationComponent,
+    UtilisateurComponent,
+    CreateFormationComponent
   ],
   imports: [
     BrowserModule,
