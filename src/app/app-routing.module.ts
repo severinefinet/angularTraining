62c0dd5d0cd8f5e88f375ebd51e403e1e49cfe74
Couzy, Guillaume <guillaumecouzy@eaton.com>
2018-12-07 16:55:39 +0100

    Système de routage, mais qui ne fonctionne pas....

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateFormationComponent } from './formation/create-formation/create-formation.component';
+import { ListFormationComponent } from './formation/list-formation/list-formation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'formation',
+    component: ListFormationComponent,
+    children: [
+      {
+        path: 'create',
+        component: CreateFormationComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
